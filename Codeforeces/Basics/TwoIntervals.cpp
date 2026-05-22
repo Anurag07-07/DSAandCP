40649be13c7f108175863ca489b4ee2cc3295d93
Anurag07-07 <adityanv8@gmail.com>
2026-05-22 21:31:16 +0530

    Codeforces Sheet 1

--- Codeforeces/Basics/TwoIntervals.cpp
+++ Codeforeces/Basics/TwoIntervals.cpp
@@ -1,13 +1,15 @@
-// //
-// // Created by anura on 22-05-2026.
-// //
-// #include <bits/stdc++.h>
-// using namespace std;
-// int main(int argc, char *argv[]) {
-//     int l1,r1,l2,r2;cin>>l1>>r1>>l2>>r2;
-//     if (l2>r1) {
-//         cout<<-1;
-//     }else if () {
-//         cout<<l2<<" "<<r1;
-//     }
-// }
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char *argv[]) {
+    int l1,r1,l2,r2;cin>>l1>>r1>>l2>>r2;
+    if (r1<l2) {
+        cout<<-1;
+    } else if (r2<l1){cout<<-1;}else if (l1<=l2 && r1>=r2) {
+        cout<<l2<<" "<<r2;
+    }else if (l1>=l2 && r1<=r2) {
+        cout<<l1<<" "<<r1;
+    }else if (r1>=l2) {
+        if (l2>=l1 && l2<=r2)cout<<l2<<" "<<r1;
+        else if (l1>=l2 && l1<=r2)cout<<l1<<" "<<r2;
+    }
+}
