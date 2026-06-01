777237058593b5545c19f61b311fb0a7c6659cf0
Anurag07-07 <adityanv8@gmail.com>
2026-06-01 14:59:27 +0530

    Recursion Practice

---
+++ Recursion/TargetSum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool targetSum(vector<int> nums,int start,int target) {
+    if (target==0) {
+        return true;
+    }
+    if (start == nums.size() && target != 0) {
+        return false;
+    }
+
+    return targetSum(nums,start+1,target) || targetSum(nums,start+1,target-nums[start]);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;cin >> t;
+
+    while (t--) {
+        int n;cin>>n;
+        vector<int> nums(n,0);
+        for(auto &x:nums)cin>>x;
+        int target;cin>>target;
+        cout<<targetSum(nums,0,target);
+    }
+
+    return 0;
+}
